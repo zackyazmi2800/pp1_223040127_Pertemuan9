68d0da92ded2eaf73147f064092ac858c2933742
zackyazmi2800 <120625416+zackyazmi2800@users.noreply.github.com>
2024-04-28 19:33:10 +0700

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan9;
+
+/**
+ *
+ * @author zack
+ */
+public class Node {
+    private int data;
+    private Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
---
+++ QueueMain.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan9;
+
+/**
+ *
+ * @author zack
+ */
+public class QueueMain {
+
+//     public static void main(String[] args) {
+//        StrukturQueue queue = new StrukturQueue();
+//
+//        // Sebelum enqueue 3x
+//        System.out.println("## Sebelum Enqueue 3x ##");
+//        System.out.println("size: " + queue.size());
+//        System.out.println("isEmpty: " + queue.isEmpty());
+//
+//        // Enqueue 3x
+//        System.out.println("### Enqueue 3x ###");
+//        queue.enqueue(1);
+//        queue.enqueue(2);
+//        queue.enqueue(3);
+//
+//        // Setelah enqueue 3x
+//        System.out.println("size: " + queue.size());
+//        System.out.println("isEmpty: " + queue.isEmpty());
+//        System.out.println("Front: " + queue.front());
+//    }
+    
+
+    public static void main(String[] args) {
+        StrukturQueue antrian = new StrukturQueue();
+
+        // Sebelum enqueue 4x
+        System.out.println("* Sebelum Enqueue 4x #**");
+        System.out.println("size: " + antrian.size());
+        System.out.println("isEmpty: " + antrian.isEmpty());
+        antrian.displayElements();
+
+        // Enqueue 4x
+        System.out.println("## Enqueue 4x #*");
+        antrian.enqueue(2);
+        antrian.enqueue(7);
+        antrian.enqueue(6);
+        antrian.enqueue(1);
+
+        // Setelah enqueue 4x
+        System.out.println("size: " + antrian.size());
+        System.out.println("isEmpty: " + antrian.isEmpty());
+        System.out.print("Elemen Queue: ");
+        antrian.displayElements();
+        System.out.println("Front: " + antrian.front());
+    }
+
+
+     
+     
+//     public static void main(String[] args) {
+//        StrukturQueue antrian = new StrukturQueue();
+//
+//        // Sebelum enqueue 3x
+//        System.out.println("## Sebelum Enqueue 3x ##");
+//        System.out.println("size: " + antrian.size());
+//        System.out.println("isEmpty: " + antrian.isEmpty());
+//
+//        // Enqueue 3x
+//        System.out.println("### Enqueue 3x ###");
+//        antrian.enqueue(1);
+//        antrian.enqueue(2);
+//        antrian.enqueue(3);
+//
+//        // Setelah enqueue 3x
+//        System.out.println("size: " + antrian.size());
+//        System.out.println("isEmpty: " + antrian.isEmpty());
+//        System.out.println("Front: " + antrian.front());
+//
+//        // Dequeue
+//        System.out.println("### Dequeue ###");
+//        int removedElement = antrian.dequeue();
+//        System.out.println("Elemen yang dihapus: " + removedElement);
+//        System.out.println("size: " + antrian.size());
+//        System.out.println("isEmpty: " + antrian.isEmpty());
+//        System.out.print("Elemen Queue: ");
+//        antrian.displayElements();
+//        System.out.println("Front: " + antrian.front());
+//    }
+}
